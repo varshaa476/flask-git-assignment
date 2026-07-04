72e9007058d619ad9ef2345715367e82162ca800
varshaa476 <varshaprabhudev03@gmail.com>
2026-07-04 04:50:56 +0000

    Add To-Do page frontend in master_1 branch

--- app.py
+++ app.py
@@ -45,6 +45,9 @@ def submit():
 def success():
     return render_template("success.html")
 
+@app.route("/todo")
+def todo():
+    return render_template("todo.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
